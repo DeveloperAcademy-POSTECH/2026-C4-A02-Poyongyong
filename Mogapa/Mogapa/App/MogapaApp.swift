fcac456315a8141235a64fa97a643e9dd9bac546
sun <sts07190@naver.com>
2026-07-19 23:26:39 +0900

    [feat/#9] SwiftData ModelContainer 구성

--- Mogapa/Mogapa/App/MogapaApp.swift
+++ Mogapa/Mogapa/App/MogapaApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MogapaApp: App {
@@ -13,5 +14,6 @@ struct MogapaApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(AppPersistence.shared)
     }
 }
