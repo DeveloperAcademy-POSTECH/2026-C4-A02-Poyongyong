bc51bd2b7ace43e0351b19977dff2cf8fed16360
빅토리아/Victoria <minson26@pos.idserve.net>
2026-07-21 00:50:30 +0900

    [feat/#22] 카드랑 라벨 추가

---
+++ Mogapa/Mogapa/Features/Home/FastSpeechSection/SpeechCategorySelector.swift
@@ -0,0 +1,18 @@
+//
+//  SpeechCategorySelector.swift
+//  Mogapa
+//
+//  Created by Minjae Son on 7/21/26.
+//
+
+import SwiftUI
+
+struct SpeechCategorySelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeechCategorySelector()
+}
