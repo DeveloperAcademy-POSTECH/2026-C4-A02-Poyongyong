a9914b29fdbf883c10ba312777b4789e0f4fed2a
빅토리아/Victoria <minson26@pos.idserve.net>
2026-07-21 01:10:28 +0900

    [feat/#22] 카드 순서 정리랑 안 보일때 뷰 추가

---
+++ Mogapa/Mogapa/Features/Home/FastSpeechSection/FastSpeechCardsPager.swift
@@ -0,0 +1,18 @@
+//
+//  FastSpeechCardsPager.swift
+//  Mogapa
+//
+//  Created by Minjae Son on 7/21/26.
+//
+
+import SwiftUI
+
+struct FastSpeechCardsPager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FastSpeechCardsPager()
+}
